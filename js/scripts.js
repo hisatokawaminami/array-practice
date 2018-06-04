e5fd5389de9404301ea16fbce4a7c9321d696b8a
Hisato Kawaminami and Swati Sahay <hisato@>
2018-06-04 09:26:42 -0700

    add array lists

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(function() {
-  var myArray=[]
+  var myArray=[];
   $("#submit").click(function() {
     var favoriteFood= $("input#food").val();
     var favoriteMusic= $("input#music").val();
@@ -7,8 +7,11 @@ $(function() {
     myArray.push(favoriteFood);
     myArray.push(favoriteMusic);
     myArray.push(favoriteGame);
-    $("#output").text(myArray);
-
+    //$("#output").text(myArray);//
+    $("#favfood").append(myArray[myArray.length - 3] + " ");
+    $("#favmusic").append(myArray[myArray.length - 2] + " ");
+    $("#favgame").append(myArray[myArray.length - 1] + " ");
+    console.log(myArray);
   });
 
 });
